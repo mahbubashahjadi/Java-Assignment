90040121859bee621ed5baa3c5b583d1a7f08bc4
mahbubashahjadi <mahbubashahjadi@gmail.com>
2026-05-12 20:53:01 +0600

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.java;
+
+
+public class Product {
+    public void main(String[] args){
+        System.out.println("id: 1011"); 
+        System.out.println("title: iphone15");
+        System.out.println("price: 1895 euros"); 
+        System.out.println("description: perfect product with best image quality"); 
+        System.out.print("category: phone"); 
+    }
+            
+}

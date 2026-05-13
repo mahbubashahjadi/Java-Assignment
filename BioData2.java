57883cb23a9031f45b3baa3084894a42f2c356c6
mahbubashahjadi <mahbubashahjadi@gmail.com>
2026-05-13 20:03:47 +0600

    Add files via upload

---
+++ BioData2.java
@@ -0,0 +1,26 @@
+
+package basicjava;
+
+
+public class BioData2 {
+    
+     public static void main(String[] args){
+        System.out.println("Name: Mahbuba Shahjadi"); 
+       System.out.println("Mother's Name: Shahnaj Akter Chowdhury"); 
+       System.out.println("Fathers Name: Billal Hossain"); 
+      System.out.println("Present Address: Khilgaon basabo,,Dhaka"); 
+       System.out.println("Parmanent Address: Akania ,,Sahedapur ,Kachua , Chandpur"); 
+        System.out.println("Date of birth: 9 October 2005"); 
+         System.out.println("Relasias: Islam"); 
+          System.out.println("Nationality: Bangladesh"); 
+           System.out.println("Education Qualification: "); 
+           
+          
+        
+        System.out.println("SSC : GPA 4.67");
+        System.out.println("Hsc : GPA 3.92");
+          System.out.println("Cse: 3rd semester running");
+ 
+     }
+    
+}
